7f3057f9ddff0777b2994835272528294b8cd7ee
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2021-10-28 19:58:08 +0900

    update for error position

--- conv_packer.go
+++ conv_packer.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"strings"
 	"sync"
@@ -19,40 +20,24 @@ import (
 	sxutil "github.com/synerex/synerex_sxutil"
 )
 
+type LatLon struct {
+	Lat float64
+	Lon float64
+	Angle float64
+}
+
 var (
 	channel           = flag.String("channel","15", "Channel for input")
 	nodesrv         = flag.String("nodesrv", "127.0.0.1:9990", "Node ID Server")
 	local           = flag.String("local","", "Local sxserv addr")
-	idlist          []uint64
-	spMap           map[uint64]*sxutil.SupplyOpts
+	posMap           map[int32]LatLon
 	mu              sync.Mutex
 	sxServerAddress string
 	supplyClt       *sxutil.SXServiceClient
 )
 
-type Channel struct {
-	Channel string `json:"channel"`
-}
-
-type MyFleet struct {
-	VehicleId  int                    `json:"vehicle_id"`
-	Status     int                    `json:"status"`
-	Coord      map[string]interface{} `json:"coord"`
-	Angle      float32                `json:"angle"`
-	Speed      int                    `json:"speed"`
-	MyServices map[string]interface{} `json"services"`
-	Demands    []int                  `json:"demands"`
-}
-
-type MyVehicle struct {
-	vehicles []*MyFleet `json:"vehicles"`
-}
-
-type MyJson map[string]interface{}
-
 func init() {
-	idlist = make([]uint64, 0)
-	spMap = make(map[uint64]*sxutil.SupplyOpts)
+	posMap = make(map[int32]LatLon)
 }
 
 // callback for each Supplu
@@ -66,47 +51,83 @@ func suppluCallback(clt *sxutil.SXServiceClient, sp *pb.Supply) {
 		if strings.HasPrefix(token[0],"NisshinEisei-OBD-") {
 			carnum := token[0][17:]
 			cn , _ := strconv.Atoi(carnum)
-			vehicle_id := 10000+ cn
-			lat, _ := strconv.ParseFloat(token[3],64)
-			lon, _ := strconv.ParseFloat(token[4],64)
-			speed,_ := strconv.ParseFloat(token[5],64)
-			angle,_ := strconv.ParseFloat(token[6],64)
-			log.Printf("CarNUm:%s, %f, %f, %f, %f", carnum, cn, lat, lon, speed, angle)
+			vehicle_id := int32(10000+ cn)
+
+			lat, err := strconv.ParseFloat(token[3],64)
+			if err != nil {
+				log.Printf("err parse lat [%s] %s",token[3],err.Error())
+				return
+			}
+			lon, err2 := strconv.ParseFloat(token[4],64)
+			if err2 != nil {
+				log.Printf("err parse lon [%s] %s",token[4],err.Error())
+				return
+			}
+			altitude,_ := strconv.ParseFloat(token[5],64)
+			speed,_ := strconv.ParseFloat(token[6],64)
+
+			lastPos, ok := posMap[vehicle_id]
+			var angle, dist float64
+			if ok { // まあだいたいでいいよね？ (北緯35 度の1度あたりの距離で計算)
+				dlat := (lastPos.Lat - lat) *  110940.5844 // 110km / degree
+				dlon := (lastPos.Lon - lon) *  91287.7885 // 91km / degree
+				// あまりに移動が小さい場合は、考える。
+				dist = math.Sqrt(dlat*dlat+dlon*dlon)
+				if speed > 0.1 && dist > 2 { // need to fix
+					angle =  math.Atan2(dlon,dlat)* 180/math.Pi +180
+				}else{
+					angle = lastPos.Angle
+				}
+			} else{
+				dist = 0
+				angle = 0
+			}
+			lastPos.Lat = lat
+			lastPos.Lon = lon
+			lastPos.Angle = angle
+			posMap[vehicle_id] = lastPos
+
+			log.Printf("CarNUm:%s, %f, %f, alt:%f spd:%f dst:%f agl %f ", carnum, lat, lon, altitude, speed, dist, angle)
+
+			if lat < 30 || lat > 40 || lon < 120 || lon  > 150 {
+				log.Printf("error too big!")
+				return
+			}
 
 					// Make Protobuf Message from JSON
-		fleet := fleet.Fleet{
-			VehicleId: int32(vehicle_id),
-			Angle:     float32(angle),
-			Speed:     int32(speed),
-			Status:    int32(0),
-			Coord: &fleet.Fleet_Coord{
-				Lat: float32(lat),
-				Lon: float32(lon),
-			},
-		}
-		out, err := proto.Marshal(&fleet)
-		if err == nil {
-			cont := pb.Content{Entity: out}
-			// Register supply
-			smo := sxutil.SupplyOpts{
-				Name:  "Map Supply",
-				Cdata: &cont,
+			fleet := fleet.Fleet{
+				VehicleId: int32(vehicle_id),
+				Angle:     float32(angle),
+				Speed:     int32(speed),
+				Status:    int32(0),
+				Coord: &fleet.Fleet_Coord{
+					Lat: float32(lat),
+					Lon: float32(lon),
+				},
 			}
-			//			fmt.Printf("Res: %v",smo)
-			_, nerr := supplyClt.NotifySupply(&smo)
-			if nerr != nil { // connection failuer with current client
+			out, err := proto.Marshal(&fleet)
+			if err == nil {
+				cont := pb.Content{Entity: out}
+				// Register supply
+				smo := sxutil.SupplyOpts{
+						Name:  "Map Supply",
+					Cdata: &cont,
+				}
+				//			fmt.Printf("Res: %v",smo)
+				_, nerr := supplyClt.NotifySupply(&smo)
+				if nerr != nil { // connection failuer with current client
 				// we need to ask to nodeidserv?
 				// or just reconnect.
-				newClient := sxutil.GrpcConnectServer(sxServerAddress)
-				if newClient != nil {
-					log.Printf("Reconnect Server %s\n", sxServerAddress)
-					supplyClt.SXClient = newClient
+					newClient := sxutil.GrpcConnectServer(sxServerAddress)
+					if newClient != nil {
+						log.Printf("Reconnect Server %s\n", sxServerAddress)
+						supplyClt.SXClient = newClient
+					}
 				}
-			}
 
+			}
 		}
 	}
-}
 }
 
 // supply for channel data
